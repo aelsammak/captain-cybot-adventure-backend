183cf8d99819dbb22550e64b0e8be25b0220c1f7
aelsammak <adielsammak@cmail.carleton.ca>
2022-10-05 23:07:00 -0400

    GitHub repo setup for Spring backend

---
+++ src/main/java/captain/cybot/adventure/backend/CaptainCybotAdventureBackendApplication.java
@@ -0,0 +1,13 @@
+package captain.cybot.adventure.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptainCybotAdventureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptainCybotAdventureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/captain/cybot/adventure/backend/CaptainCybotAdventureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package captain.cybot.adventure.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptainCybotAdventureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
