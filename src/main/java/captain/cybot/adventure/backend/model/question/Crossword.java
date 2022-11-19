259fbdc6cfe96fadbbb5562d01bd9cca1674da68
aelsammak <adielsammak@cmail.carleton.ca>
2022-11-18 22:08:48 -0500

    Sprint4: Allowed Hibernate to map Java String[] & String[][]

--- src/main/java/captain/cybot/adventure/backend/model/question/Crossword.java
+++ src/main/java/captain/cybot/adventure/backend/model/question/Crossword.java
@@ -1,18 +1,47 @@
 package captain.cybot.adventure.backend.model.question;
 
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
+@TypeDefs({
+        @TypeDef(
+                name = "string-array",
+                typeClass = StringArrayType.class
+        )
+})
 public class Crossword extends Question {
+
+    @Type(type = "string-array")
+    @Column(
+            name = "crossword_block",
+            columnDefinition = "text[][]"
+    )
     private String[][] crosswordBlock;
+
+    @Type(type = "string-array")
+    @Column(
+            name = "hints",
+            columnDefinition = "text[]"
+    )
     private String[] hints;
+
+    @Type(type = "string-array")
+    @Column(
+            name = "answers",
+            columnDefinition = "text[]"
+    )
     private String[] answers;
 
     public Crossword(String[][] crosswordBlock, String[] hints, String[] answers) {
--- src/main/java/captain/cybot/adventure/backend/model/question/WordSearch.java
+++ src/main/java/captain/cybot/adventure/backend/model/question/WordSearch.java
@@ -1,22 +1,45 @@
 package captain.cybot.adventure.backend.model.question;
 
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
+@TypeDefs({
+        @TypeDef(
+                name = "string-array",
+                typeClass = StringArrayType.class
+        )
+})
 public class WordSearch extends Question {
-    private String[][] searchArray;
-    private String[] answer;
 
-    public WordSearch(String[][] searchArray, String[] answer) {
+    @Type(type = "string-array")
+    @Column(
+            name = "search_array",
+            columnDefinition = "text[][]"
+    )
+    private String[][] searchArray;
+
+    @Type(type = "string-array")
+    @Column(
+            name = "answers",
+            columnDefinition = "text[]"
+    )
+    private String[] answers;
+
+    public WordSearch(String[][] searchArray, String[] answers) {
         super("WORD_SEARCH");
         this.searchArray = searchArray;
-        this.answer = answer;
+        this.answers = answers;
     }
 }
